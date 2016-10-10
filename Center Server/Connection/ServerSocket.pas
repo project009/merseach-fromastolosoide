4f8d5483f636336990c6a409bff692278e2b3c8f
project009 <victorkengaloca@yopmail.com>
2016-10-10 19:50:22 -0300

    Update ServerSocket.pas

--- Center Server/Connection/ServerSocket.pas
+++ Center Server/Connection/ServerSocket.pas
@@ -146,8 +146,8 @@ begin
   Loading.SquareLoading[2]:='SquareObject.lua';
   Loading.SquareLoading[3]:='Square3DObject.lua';
   Loading.AddHack('Teste.dll');
-  Loading.AddCheck('mainxx.exe','A42B77BEEECE321B19257C7EE803548B9DFD7A42');
-  Loading.AddCheck('script.kom','5E1959836BDCCE737EA85BCC781B7952274D55B1');
+  //Loading.AddCheck('mainxx.exe','A42B77BEEECE321B19257C7EE803548B9DFD7A42');
+  //Loading.AddCheck('script.kom','5E1959836BDCCE737EA85BCC781B7952274D55B1');
   Loading.GuildMark:='http://gcreborn.com/GuildMark/';
   Unknown:=TUnknown.Create;
   Players:=TList<TPlayer>.Create;
