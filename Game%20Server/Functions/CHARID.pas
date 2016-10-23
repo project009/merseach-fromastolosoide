170b96891747be521188fda709c75ec04485cfcb
capcom_x9@hotmail.com <capcom_x9@hotmail.com>
2016-10-22 22:32:16 -0200

    Add files via upload

---
+++ Game%20Server/Functions/CHARID.pas
@@ -0,0 +1,37 @@
+unit CHARID;
+
+interface
+uses Data.DB, DBCon, System.SysUtils,AccountInfo;
+type
+  TCHARID = class
+    private
+      MySQL: TQuery;
+      AccInfo: TAccountInfo;
+    public
+      ID: Integer;
+      CHARID: AnsiString;
+      constructor Create(MySQL: TQuery;AccInfo: TAccountInfo);
+      procedure Update;
+  end;
+implementation
+
+constructor TCHARID.Create(MySQL: TQuery;AccInfo: TAccountInfo);
+Begin
+    Self.ID:=Accinfo.ID;
+    Self.MySQL:=MySQL;
+    MySQL.SetQuery('SELECT CHARID FROM characters WHERE ID =:ID');
+    MySQL.AddParameter('ID',AnsiString(IntToStr(ID)));
+    MySQL.Run(1);
+   if MySQL.Query.IsEmpty = False then begin
+    CHARID:=MySQL.Query.Fields[0].AsAnsiString;
+   end;
+End;
+procedure TCHARID.Update;
+begin
+  MySQL.SetQuery('UPDATE characters SET CHARID = :CHARID WHERE ID = :ID');
+  MySQL.AddParameter('CHARID',AnsiString(CHARID));
+  MySQL.AddParameter('ID',AnsiString(IntToStr(ID)));
+  MySQL.Run(2);
+end;
+
+end.
